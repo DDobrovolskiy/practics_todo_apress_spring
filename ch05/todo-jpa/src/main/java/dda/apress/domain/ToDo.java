9ef6d282eb6f6a12930b8bc32d4bd77309b6402f
DDobrovolskiy <dda.90@mail.ru>
2022-02-16 13:00:47 +0500

    JPA Spring

---
+++ ch05/todo-jpa/src/main/java/dda/apress/domain/ToDo.java
@@ -0,0 +1,44 @@
+package dda.apress.domain;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Data
+@NoArgsConstructor
+public class ToDo {
+    @Id
+    @GeneratedValue(generator = "system-uuid")
+    @GenericGenerator(name = "system-uuid", strategy = "uuid")
+    private String id;
+    @NotNull
+    @NotBlank   //Не пустое
+    private String description;
+    @Column(insertable = true, updatable = false)
+    private LocalDateTime created;
+    private LocalDateTime modified;
+    private boolean completed;
+
+    public ToDo(String description) {
+        this();
+        this.description = description;
+    }
+
+    @PrePersist
+    void onCreate() {
+        this.setCreated(LocalDateTime.now());
+        this.setModified(LocalDateTime.now());
+    }
+
+    @PreUpdate
+    void onUpdate() {
+        this.setModified(LocalDateTime.now());
+    }
+}
